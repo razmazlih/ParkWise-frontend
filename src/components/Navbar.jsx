89c48652f5d5ae45dedd8b56b902aab079b69043
Raz <retraz123@gmail.com>
2024-11-29 08:14:53 +0200

    add link to Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,19 @@
+import { Link } from 'react-router-dom';
+import './styles/Navbar.css';
+
 function Navbar() {
     return (
         <div>
             <ul>
-                <li>see free parking spots</li>
-                <li>add parking spots</li>
+                <Link to="/" className="parking-link">
+                    <li>Home</li>
+                </Link>
+                <Link to="/parking-areas" className="parking-link">
+                    <li>Free parking spots</li>
+                </Link>
+                <Link to="/parking-spots" className="parking-link">
+                    <li>Parking spots</li>
+                </Link>
             </ul>
         </div>
     );
